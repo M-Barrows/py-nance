071d262195544580dc0793ea90156eee72f4c2ac
Michael Barrows <michael.barrows.a@gmail.com>
2023-09-21 00:25:40 -0400

    Starting a manual re-write
    This just gets the main function(s) down

--- app.py
+++ app.py
@@ -3,108 +3,31 @@
 
 # Import the necessary modules
 import dash
-from dash import dcc, html
+from dash import dcc, html, callback
 from dash.dependencies import Input, Output, State
-
+import dash_ag_grid as dag
+import pandas as pd
+import plotly.express as px
 import dash_bootstrap_components as dbc
+from libs import helper_funcs as hf
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
 app.title = 'Py-Nance'
 
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink('CDs', href='/cds'))
-    ],
-    brand='Py-Nance',
-    brand_href='/',
-    color='primary',
-    dark=True,
-    fluid=True
-)
-
-footer = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            html.P([
-                'Made with ',
-                html.Span('❤️', className='text-danger'),
-                ' by ',
-                html.A('Code and Coffee', href='https://blog.codecoffee.org', target='_blank')
-            ], className='text-center')
-        )
-    ),
-    fluid=True,
-    className='fixed-bottom bg-light'
-)
-
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    navbar,
-    dbc.Container(id='page-content', fluid=True),
-    footer
+    html.H1(children="PyNance", style={'textAlign':'center'}),
+    dcc.RangeSlider(id='range-slider',min=1,max=100),
+    dcc.Textarea (
+        id='data-grid'
+        )
 ])
 
-@app.callback(
-    Output('page-content', 'children'),
-    [Input('url', 'pathname')]
+@callback(
+    Output('data-grid','value'),
+    Input('range-slider','value')
 )
-def display_page(pathname):
-    if pathname == '/cds':
-        return cds_layout
-    else:
-        return main_layout
-
-main_layout = dbc.Container([
-    dbc.Container([
-        html.H1('Py-Nance', className='display-3 text-center'),
-        html.P(
-            'A collection of open-source tools to help you understand your financial future and answer some of the "what if?" questions.',
-            className='lead text-center'
-        ),
-        html.Hr(className='my-2'),
-        dbc.Row([
-            dbc.Col([
-                html.A(
-                dbc.Card([
-                    dbc.CardImg(src='/assets/cd.png', top=True),
-                    dbc.CardBody([
-                        html.H4('How much will your CD be worth?', className='card-title')
-                    ])
-                ]), href="/cds")
-            ], width=6),
-
-        ])
-    ])
-], fluid=True)
-
-cds_layout = dbc.Container([
-    dbc.Row([
-        dbc.Col(html.Label('APY:'), width=2),
-        dbc.Col(dcc.Input(id='apy', type='number', min=0, step=0.01), width=4)
-    ]),
-    dbc.Row([
-        dbc.Col(html.Label('Term Length (months):'), width=2),
-        dbc.Col(dcc.Input(id='term_length', type='number', min=0, step=1), width=4)
-    ]),
-    dbc.Row([
-        dbc.Col(html.Label('Starting Value:'), width=2),
-        dbc.Col(dcc.Input(id='starting_value', type='number', min=0, step=0.01), width=4)
-    ]),
-    html.Br(),
-    html.Div(id='output')
-], fluid=True)
-
-@app.callback(
-    Output('output', 'children'),
-    [Input('apy', 'value'), Input('term_length', 'value'), Input('starting_value', 'value')]
-)
-def calculate_final_value(apy, term_length, starting_value):
-    if apy is not None and term_length is not None and starting_value is not None:
-        final_value = starting_value * (1 + apy/100) ** (term_length/12)
-        return f'Final Value: ${final_value:,.2f}'
-    else:
-        return 'Please enter values for APY, term length, and starting value.'
-
+def cold_start(value):
+    return str(hf.compound_interest(20000,0.0425,12,(9/12)))
 # Run the app on port 8080
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port=8080, debug=True)
---
+++ libs/helper_funcs.py
@@ -0,0 +1,55 @@
+import polars as pl
+
+def compound_interest(p:float,r:float,n:int,t:float=1):
+    """
+    `p : float`, Initial principal balance in the account
+    `r : float`, Growth rate (usually APY) of the investment
+    `n : int`, Number of times the investment compounds per year (t)
+    `t : float` (optional), Number of cycles the investment will remain untouched. If the investment period is less 
+        than 1 year, adjust this accordingly. eg: A monthly compounding investment that is held for 9 months would 
+        have <n = 12, t = (9/12)>.
+
+    example:
+    >>> compound_interest(20000,0.0425,12,(9/12))
+    20646.606281143224
+    """
+    return p*(1+r/n)**(n*t)
+
+def make_compound_reinvestment_df(p:float,r:float,n:int,t:float=1,n_terms:int=1,percent_reinvest:int=100): 
+    """
+    `p : float`, Initial principal balance in the account
+    `r : float`, Growth rate (usually APY) of the investment
+    `n : int`, Number of times the investment compounds per year (t)
+    `t : float` (optional), Number of cycles the investment will remain untouched. If the investment period is less 
+        than 1 year, adjust this accordingly. eg: A monthly compounding investment that is held for 9 months would 
+        have <n = 12, t = (9/12)>.
+    `n_terms : int` (optional), Number of times this investment will be renewed - reinvesting some percent (`percent_reinvest`) each time.
+    `percent_reinvest : int`, percent (1-100) of the investment that should be reinvested each n_terms 
+
+    Usage examples:
+    >>> make_compound_reinvestment_df(20000,0.0425,12,(9/12),3,50)
+    shape: (4, 4)
+    ┌──────┬────────────────┬───────────────┬──────────────┐
+    │ Term ┆ Term_Principal ┆ Term_Interest ┆ Total_Value  │
+    │ ---  ┆ ---            ┆ ---           ┆ ---          │
+    │ i64  ┆ f64            ┆ f64           ┆ f64          │
+    ╞══════╪════════════════╪═══════════════╪══════════════╡
+    │ 0    ┆ 20000.0        ┆ 0.0           ┆ 20000.0      │
+    │ 1    ┆ 10323.303141   ┆ 646.606281    ┆ 20646.606281 │
+    │ 2    ┆ 5328.529387    ┆ 333.755633    ┆ 20980.361914 │
+    │ 3    ┆ 2750.401208    ┆ 172.273029    ┆ 21152.634942 │
+    └──────┴────────────────┴───────────────┴──────────────┘
+    """
+    term_principal = [p]
+    interest_earned = [0]
+    term_interest = [0]
+    total_value = [p]
+    terms_remaining = n_terms
+    while terms_remaining > 0:
+        term_interest.append(compound_interest(term_principal[-1],r,n,t)-term_principal[-1])
+        interest_earned.append(term_interest[-1])
+        total_value.append(total_value[-1] + term_interest[-1])
+        term_principal.append((term_principal[-1] + term_interest[-1]) * (percent_reinvest/100))
+        terms_remaining -= 1
+    df = pl.DataFrame({'Term':list(range(n_terms+1)), 'Term_Principal':term_principal,"Term_Interest":term_interest,"Total_Value":total_value})
+    return df

bc47b78b12521289fb12b168ba4ea6d9ec1d157a
Michael Barrows <michael.barrows.a@gmail.com>
2023-09-23 09:07:55 -0400

    debug off

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ app.layout = dbc.Container([
 
 # Run the app on port 8080
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(host='0.0.0.0', port=8080, debug=False)
